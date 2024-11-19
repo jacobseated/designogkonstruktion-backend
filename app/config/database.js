d8e32d3943b90e8458286be2506cb345bdf4f83e
Jacob <jacobseated@gmail.com>
2024-11-19 09:27:53 +0100

    Users endpoint is working...

--- app/config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host: 'wriggleflap.beamtic.net',
-    database: 'venner_for_livet',
-    username: 'wobblecrash',
-    password: 'RSVkjFshIzwxqrTSS2gF88JNyGRA',
-    dialect: 'mariadb',
-    ssl: true
-};
--- app/db/sequelize.js
+++ app/db/sequelize.js
@@ -1,13 +1,9 @@
 const { Sequelize } = require('sequelize');
-const dbConfig = require('../config/database');
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: 'mariadb',
-    dialectOptions: {
-        ssl: true
-    },
-    logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: process.env.DB_SSL === 'true' ? { ssl: true } : {},
 });
 
 module.exports = sequelize;
