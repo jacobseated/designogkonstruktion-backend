882c14a779fdc10f6e96b909ec34c1f38b834b59
Jacob <jacobseated@gmail.com>
2024-12-11 05:23:12 +0100

    Removed expirey data from event

---
+++ app/db/event/deleteOldForumMessagesEvent.js
@@ -0,0 +1,25 @@
+const sequelize = require('../sequelize');
+
+// Denne kode bliver kaldt i server.js
+// Det er vigtigt, fordi vores database-tabeller automatisk bliver oprettet af Sequelize når vi starter serveren
+const deleteOldForumMessagesEvent = async () => {
+    const eventSQL = `
+    CREATE EVENT IF NOT EXISTS delete_old_forum_messages
+    ON SCHEDULE EVERY 1 DAY
+    STARTS '2024-11-29 00:00:00'
+    ON COMPLETION PRESERVE
+    ENABLE
+    COMMENT 'Command deletes 3 month old messages.'
+    DO DELETE FROM forum WHERE message_created < NOW() - INTERVAL 3 MONTH;
+  `;
+
+  // Kør koden og tjek om der var fejl
+  try {
+    await sequelize.query(eventSQL);
+    console.log('Event created or already exists.');
+  } catch (error) {
+    console.error('Error creating event:', error);
+  }
+  };
+  
+  module.exports = deleteOldForumMessagesEvent;
